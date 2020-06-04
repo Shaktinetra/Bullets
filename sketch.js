542f2ca09138f599f57883c02a0e9098a5dc2327
Shaktinetra <65832818+Shaktinetra@users.noreply.github.com>
2020-06-04 19:49:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,18 +6,17 @@ function setup() {
   speed = random(223, 321);
   
   weight = random(30, 52);
+  
+  thickness = random(22, 83);
 
   damage = (0.5 * speed * weight * speed) / (thickness * thickness * thickness)
 
   bullet = createSprite(50, 200, 10, 5);
   bullet.shapeColor = "white";
+  bullet.velocityX = speed;
   
   wall = createSprite(1200, 200, thickness, height/2);
   wall.shapeColor = (80, 80, 80);
-
-  bullet.velocityX = speed;
-
-  thickness = random(22, 83);
 }
 
 function draw() {
@@ -29,11 +28,11 @@ function draw() {
    bullet.velocityX = 0;
 
    if (damage > 10) {
-     wall.shapeColor = color(255, 0, 0);
+     wall.shapeColor = "red";
    }
 
    if (damage < 10) {
-     wall.shapeColor = color(0, 255, 0);
+     wall.shapeColor = "green";
    }
  }
 
