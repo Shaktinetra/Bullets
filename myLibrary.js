6fd43c09e7b31e13aa2e99798d5baab98f1cc807
Shaktinetra <senthilkumarshaktinetra@gmail.com>
2020-07-15 17:52:19 -0400

    Edited

--- myLibrary.js
+++ myLibrary.js
@@ -40,6 +40,7 @@ function hasCollided(object1, object2) {
     object1RightEdge = object1.x + object1.width;
     object2LeftEdge = object2.x;
     if (object1RightEdge>=object2LeftEdge) {
+        object1.velocityX = 0;
         return true;
     }
         return false;
--- sketch.js
+++ sketch.js
@@ -3,29 +3,26 @@ var bullet, wall, speed, weight, damage, thickness;
 function setup() {
   createCanvas(1600,400);
 
-  speed = random(223, 321);
-  
-  weight = random(30, 52);
-  
-  thickness = random(22, 83);
+  speed = random(220, 340);
+  weight = random(30, 52);  
+  thickness = random(50, 100);
 
-  damage = (0.5 * speed * weight * speed) / (thickness * thickness * thickness)
-
-  bullet = createSprite(50, 200, 10, 5);
+  bullet = createSprite(50, 200, 20, 20);
   bullet.shapeColor = "white";
   bullet.velocityX = speed;
-  
+  console.log(speed);
+
   wall = createSprite(1200, 200, thickness, height/2);
 }
 
 function draw() {
   background(0); 
   
-  console.log(wall.x);
-  
- if (isTouching(bullet, wall)) {
+ if (hasCollided(bullet, wall)) {
    bullet.velocityX = 0;
-   
+
+   damage = (0.5 * speed * weight * speed) / (thickness * thickness * thickness);
+
    if (damage >= 10) {
      wall.shapeColor = "red";
    }
@@ -33,7 +30,8 @@ function draw() {
    if (damage < 10) {
      wall.shapeColor = "green";
    }
+   bullet.collide(wall);
  }
 
-  drawSprites();
-}
+ drawSprites();
+}  
